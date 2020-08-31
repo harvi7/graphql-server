1a429af5ee99bf30f5a54decc9a98cfa2cca0c34
Harsh Virani <iamharshvirani7@gmail.com>
2020-08-30 23:55:53 -0700

    Likes & Comments

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/typeDef');
+const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
+const pubsub = new PubSub();
+
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ req })
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
 });
 
-mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('MongoDB Connected');
-        return server.listen({port: 5000 });
-    }).then(res => {
-        console.log(`Server running at ${res.url}`)
-    });
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
